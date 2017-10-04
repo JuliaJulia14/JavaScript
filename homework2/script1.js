5e75a72417cf28a2b46cd9b99811c7e84c9e531e
JuliaVoitushkevich <julia.smile.v@gmail.com>
2017-10-05 02:56:22 +0300

    Домашнее задание №2

--- homework2/script1.js
+++ homework2/script1.js
@@ -20,7 +20,7 @@ function compareNumeric(a, b) {
 var s=Number(prompt("Enter the average value")); 
 var p=Number(prompt("Enter the deviation")); 
 arr=generate(s, p);
-document.write("<br\/> The average is "+ arr.pop());
+document.write("<br\/> The average is "+ (arr.pop()).toFixed(2));
 arr.sort(compareNumeric);
 document.write("<br\/>Sorted array:<br\/>");
 for (var i = 0; i < arr.length; i++) {
